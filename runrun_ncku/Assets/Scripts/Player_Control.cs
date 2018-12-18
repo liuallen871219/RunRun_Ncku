313e59627f42e9c9a0bfe5138b4460bee85ec3c5
吳添毅 <asd887722456@gmail.com>
2018-12-18 22:36:06 +0800

    KP

---
+++ runrun_ncku/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (transform.position.y <= -1.2f)
+                transform.position = transform.position + new Vector3(0, 1.5f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (transform.position.y >= -3.5f)
+                transform.position = transform.position + new Vector3(0, -1.5f, 0);
+        }
+    }
+}
