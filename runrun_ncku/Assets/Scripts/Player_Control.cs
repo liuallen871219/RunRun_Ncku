250bcf6e9cbf8e3432af4ccecfa5e3b0d8074f15
liuallen871219 <40296275+liuallen871219@users.noreply.github.com>
2018-12-21 00:08:18 +0800

    add background

--- runrun_ncku/Assets/Scripts/Player_Control.cs
+++ runrun_ncku/Assets/Scripts/Player_Control.cs
@@ -13,13 +13,13 @@ public class Player_Control : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            if (transform.position.y <= -1.2f)
-                transform.position = transform.position + new Vector3(0, 1.5f, 0);
+            if (transform.position.y <= -3.4f)
+                transform.position = transform.position + new Vector3(0, 0.5f, 0);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            if (transform.position.y >= -3.5f)
-                transform.position = transform.position + new Vector3(0, -1.5f, 0);
+            if (transform.position.y >= -3.9f)
+                transform.position = transform.position + new Vector3(0, -0.5f, 0);
         }
     }
 }
---
+++ runrun_ncku/Assets/Scripts/background_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public float speed;
+    Vector2 offset = Vector2.zero;
+    // Use this for initialization
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
