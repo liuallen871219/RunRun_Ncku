5f306df665f5a78adc05f9eaab2618103367c90e
吳添毅 <asd887722456@gmail.com>
2018-12-21 02:19:56 +0800

    generate coin

---
+++ runrun_ncku/Assets/Scripts/coin_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("coinMove", 0.01f, 0.05f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < -6.0f)
+            Destroy(transform.gameObject);
+        
+	    	
+	}
+    public float move_speed = -0.02f;
+    void coinMove()
+    {
+        transform.position = transform.position + new Vector3(move_speed, 0, 0);
+    }
+}
---
+++ runrun_ncku/Assets/Scripts/generateCoin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateCoin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("generate", 5, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void generate()
+    {
+        Instantiate(coin).transform.position = new Vector3(5, Random.Range(-1, 1) * 0.5f + (-4.28f), 0);
+    }
+    public GameObject coin;
+}
