db9c8731f4b1ad12263df4a2b45f56196c52580b
liuallen871219 <40296275+liuallen871219@users.noreply.github.com>
2018-12-25 17:15:36 +0800

    add ghost

---
+++ runrun_ncku/Assets/Scripts/Extinguishing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extinguishing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
